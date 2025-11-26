c588518a8752e43ac368bfd70f4507f23b7f93d7
Sanjana0412 <sanjanak.ai23@bmsce.ac.in>
2025-11-26 15:19:44 +0530

    ami dataset

--- load_datasets.py
+++ load_datasets.py
@@ -1,11 +1,16 @@
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
+import os
 
-ds = load_dataset(
-    "csv",
-    data_files={
-        "train": r"data\raw\samsum\train.csv",
-        "validation": r"data\raw\samsum\validation.csv",
-        "test": r"data\raw\samsum\test.csv",
-    }
-)
-print(ds)
+# 1. Load the AMI dataset (IHM config)
+ds = load_dataset("edinburghcstr/ami", "ihm")
+
+print(ds)   # to check splits
+
+# 2. Save it into your raw folder
+save_path = r"data/raw/AMI"
+os.makedirs(save_path, exist_ok=True)
+
+ds.save_to_disk(save_path)
+
+print(f"\n✅ AMI dataset saved to: {save_path}")
+
